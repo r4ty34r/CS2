a57485d3b141f545d7e06f3e84d1d4f29e3cc118
r4ty34r <83795023+r4ty34r@users.noreply.github.com>
2022-11-29 23:14:48 -0500

    Update Coursedriver.cpp
    updated tests

--- Coursedriver.cpp
+++ Coursedriver.cpp
@@ -1,5 +1,7 @@
 #include "Course.h"
+#include "Student.h"
 #include <iostream> 
+#include <vector> 
 
 using namespace std;
 
@@ -13,13 +15,16 @@ void pointerprint (Student* aPointer)
 
 //OB: created driver program 
 int main(){
-    Student *myPointer = new Student;
+
+    cout << "program compiled successfully!" << endl;
+    
+    Student *myPointer = new Student; 
     string myFirst = "Joe";
     string myLast = "Smith";
 
     
-
-    Course newCourse; //initialize a new course 
+    Course newCourse;
+    cout << "course initialized successfully!" << endl;
 
     newCourse.setName("Computer Science II"); //assign a new name 
     newCourse.setCourseNum(2000); //assign a new course number 
@@ -30,18 +35,30 @@ int main(){
     anotherOne.setCourseNum(9000); // assign a new number 
     anotherOne.printCourse();   //output course information 
 
-   myPointer->setStudentInfo("Archaeology", myFirst, myLast);
+    myPointer->setStudentInfo("Archaeology", myFirst, myLast);
 
     newCourse.addToRoster(myPointer);
 
     myPointer->printStudent();
-
+    cout << endl;
+    cout << "Printing the roster names now: " << endl;
     newCourse.printRosterNames();
 
+    Student * anotherPointer = new Student; 
+
+    string newfname, newlname, major; //variables for the next student object 
+    newfname = "John";
+    newlname = "Doe";
+    major = "Political Science";
 
 
-    
- 
+    anotherPointer->setStudentInfo(major, newfname, newlname);
+    newCourse.addToRoster(anotherPointer);
+
+    cout << "Calling print roster a second time: " << endl;
+    newCourse.printRosterNames();
+
+    cout << "\nDone testing." << endl;
 
     return 0;
 }
